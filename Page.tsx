72a3b1d2debbe6acfa239673f4f333181df48220
Mordecai Kiplangat <humblemorde@gmail.com>
2026-07-08 13:13:59 +0300

    Create Home component for landing page

---
+++ Page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-green-500">WINBET</h1>
+      <p>Real Money Betting KE</p>
+      <a href="/login" className="bg-green-600 p-3 rounded mt-4 inline-block">Login / Sign Up</a>
+    </div>
+  )
+}
